77ab85bb05d2dd6e40cfc058174bca9e11312fd4
Jody Bailey <jodybailey93@gmail.com>
2019-07-06 18:54:56 -0500

    Added cart icon and cart-dropdown components to the application, also implemented cart reducer.

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,4 @@
-import { SET_CURRENT_USER } from '../../constants/reducers.constants';
+import { SET_CURRENT_USER } from './user.actions.types';
 
 export const setCurrentUser = user => ({
     type: SET_CURRENT_USER,
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,18 +1,19 @@
-import { SET_CURRENT_USER } from '../../constants/reducers.constants';
+import { SET_CURRENT_USER } from './user.actions.types';
 
 const INITIAL_STATE = {
-    currentUser: null
-}
+  currentUser: null
+};
 
 const userReducer = (state = INITIAL_STATE, action) => {
-    switch(action.type) {
-        case SET_CURRENT_USER:
-            return {
-                ...state, currentUser: action.payload
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: action.payload
+      };
+    default:
+      return state;
+  }
+};
 
 export default userReducer;

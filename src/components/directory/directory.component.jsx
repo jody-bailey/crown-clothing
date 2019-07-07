c1ffd95e06ffe72fbd4bf09a1bc656edd733a7a6
Jody Bailey <jodybailey93@gmail.com>
2019-07-07 18:38:33 -0500

    Saving my current files before moving on to another task.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
 import MenuItem from '../menu-item/menu-item.component';
-import { sections } from './directory.data';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
 import './directory.styles.scss';
 
-class Directory extends Component {
-    constructor() {
-        super();
-        this.state = {
-            sections: sections
-        }
-    }
+const Directory = ({ sections }) => {
+  return (
+    <div className='directory-menu'>
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
+      ))}
+    </div>
+  );
+};
 
-    render() {
-        return (
-            <div className='directory-menu'>
-                {
-                    this.state.sections.map(({ id, ...otherSectionProps }) => (
-                        <MenuItem key={id} {...otherSectionProps} />
-                    ))
-                }
-            </div>
-        );
-    }
-}
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);

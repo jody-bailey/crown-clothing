1023431cc50e4815d0a9a86898dc8e5b61f56517
Jody Bailey <jodybailey93@gmail.com>
2019-07-07 16:08:39 -0500

    Updates

---
+++ precache-manifest.4ad1724781fca0718b9840ca4d42d08f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8698c1b5e82de3ac79b489cbfe02aa87",
+    "url": "/crown-clothing/index.html"
+  },
+  {
+    "revision": "a4811117de22df177d82",
+    "url": "/crown-clothing/static/css/main.c60a24f2.chunk.css"
+  },
+  {
+    "revision": "c8913315a28249069979",
+    "url": "/crown-clothing/static/js/2.9ef4fc13.chunk.js"
+  },
+  {
+    "revision": "a4811117de22df177d82",
+    "url": "/crown-clothing/static/js/main.973604a1.chunk.js"
+  },
+  {
+    "revision": "0cc0bad3e59a3f3f3964",
+    "url": "/crown-clothing/static/js/runtime~main.1a01c768.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crown-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crown-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
